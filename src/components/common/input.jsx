e28f9d7919e90f800c91ca0686ae5cf1f675f0b4
colehoux <charlo.lehoux@gmail.com>
2021-04-12 21:18:46 -0400

    Basic validation (onSubmit and onChange of inputs)

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Input = ({ name, label, value, onChange }) => {
+const Input = ({ name, label, value, error, onChange }) => {
     return (
         <div className="form-group">
             <label htmlFor={name}>{label}</label>
@@ -10,9 +10,9 @@ const Input = ({ name, label, value, onChange }) => {
                 name={name}
                 id={name}
                 type="text"
-                className="form-control"
-                autoFocus
+                className={`form-control ${error ? "is-invalid" : ""}`}
             />
+            {error && <div className="invalid-feedback">{error}</div>}
         </div>
     );
 };
