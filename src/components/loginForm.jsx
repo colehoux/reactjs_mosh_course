a8da0b3147ba043100f028b9e5b7daf4edfedd07
colehoux <charlo.lehoux@gmail.com>
2021-05-31 21:40:25 -0400

    Storing the JWT

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -19,7 +19,9 @@ class LoginForm extends Form {
         const { data } = this.state;
 
         try {
-            await authService.login(data.username, data.password);
+            const { data: jwt } = await authService.login(data.username, data.password);
+            localStorage.setItem("token", jwt);
+            this.props.history.push("/");
         } catch (ex) {
             if (ex.response && ex.response.status === 400) {
                 const errors = { ...this.state.errors };
