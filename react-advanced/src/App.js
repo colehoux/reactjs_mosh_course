d20cfb7a8b955a07c96e05d0d08918c28b9ab304
colehoux <charlo.lehoux@gmail.com>
2021-06-07 23:32:08 -0400

    Hooks useState and useEffect

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Movie from "./hoc/Movie";
+import Counter from "./hooks/Counter";
 
 function App() {
-    return <Movie id="12334567" />;
+    // return <Movie id="12334567" />;
+    return <Counter />;
 }
 
 export default App;
--- react-advanced/src/hooks/Counter.jsx
+++ react-advanced/src/hooks/Counter.jsx
@@ -1,7 +1,24 @@
-import React, { useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 export default function Counter(props) {
-    const [count, setState] = useState(0);
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
 
-    return <div>Counter : {}</div>;
+    useEffect(() => {
+        document.title = `${name} has clicked ${count} times!`;
+
+        return () => {
+            //cleanup
+        };
+    }, [count, name]);
+
+    return (
+        <Fragment>
+            <input type="text" onChange={(e) => setName(e.target.value)} />
+            <div>
+                {name} has clicked {count} times
+            </div>
+            <button onClick={() => setCount(count + 1)}>Yeet up</button>
+        </Fragment>
+    );
 }
