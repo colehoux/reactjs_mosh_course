35012d6afb69988419100e3c8ad61938b1541c84
colehoux <charlo.lehoux@gmail.com>
2021-06-07 23:12:20 -0400

    Reorganized projects structure

---
+++ react-advanced/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Movie from "./hoc/Movie";
+
+function App() {
+    return <Movie id="12334567" />;
+}
+
+export default App;
---
+++ react-advanced/src/hooks/Counter.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export default function Counter(props) {
+    const [count, setState] = useState(0);
+
+    return <div>Counter : {}</div>;
+}
